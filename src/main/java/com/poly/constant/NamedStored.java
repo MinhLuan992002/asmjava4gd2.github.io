c9e4070f43940dbc04feff892716768d4238fe69
Minh Luan <minhluan1875@gmail.com>
2023-04-08 15:40:39 +0700

    commit

---
+++ src/main/java/com/poly/constant/NamedStored.java
@@ -0,0 +1,5 @@
+package com.poly.constant;
+
+public class NamedStored {
+	public static final String FIND_USER_BY_HREF = "User.FindListLikedByVideoHref";
+}
---
+++ src/main/java/com/poly/constant/SessionAtt.java
@@ -0,0 +1,5 @@
+package com.poly.constant;
+
+public class SessionAtt {
+		public static final String CURRENT_USER = "currentUser";
+}
